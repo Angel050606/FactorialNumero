baed07b972c0b07e2ccc313e452613e84c4d5617
angel <adlf0506@gmail.com>
2026-09-08 12:38:27 -0600

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -9,3 +9,15 @@ function factorial(n) {
 
 // Ejemplo de uso:
 console.log(factorial(5)); // Resultado: 120
+
+function factorial2(n) {
+  // Caso base: el factorial de 0 y 1 es 1
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  // Caso recursivo: n * factorial(n - 1)
+  return n * factorial(n - 1);
+}
+
+// Ejemplo de uso:
+console.log(factorial2(5)); // Resultado: 120
